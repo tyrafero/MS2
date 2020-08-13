c3c37a313714afc2e08fbb5e643254d5ddb936c4
tyrafero <lehanhunter@gmail.com>
2020-08-13 13:18:10 +0545

    Register and new activity

--- app/src/main/java/com/example/ms2/Register.java
+++ app/src/main/java/com/example/ms2/Register.java
@@ -15,23 +15,30 @@ public class Register extends AppCompatActivity {
     private Spinner spinner;
     private EditText editText;
 
+    /* Register your number for OTP*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        /*For countries choosing option with their code number*/
         spinner = findViewById(R.id.spinnerCountries);
+
+        /*For country code, I have downloaded a java file containing all the names of the countries with their respective Country codes */
         spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
 
         editText = findViewById(R.id.editTextPhone);
+
+
+        /* This is what happens when you enter a number and select the country code
+        * setOnClickListener verifies what to do when you press the Button*/
 
         findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String code = CountryData.countryAreaCodes[spinner.getSelectedItemPosition()];
-
+                /* Changes number to string*/
                 String number = editText.getText().toString().trim();
-
+                /*Checks if the provided number is valid or not*/
                 if (number.isEmpty() || number.length() < 10) {
                     editText.setError("Valid number is required");
                     editText.requestFocus();
@@ -52,6 +59,8 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        /*This function focuses on sending the otp code to the number you have entered to register*/
 
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             Intent intent = new Intent(this, Otp.class);
